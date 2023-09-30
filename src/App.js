84cc3f502ea4d2e744621f6e94af89a3cb6dac16
abhay-raizada <toabhayraizada@gmail.com>
2023-09-30 20:07:50 +0530

    Fix footer overlap

--- src/App.js
+++ src/App.js
@@ -9,10 +9,22 @@ function App() {
   return (
     <div className="App">
       <Header selected="Create Form" />
-      <div style={{ padding: "5%", maxWidth: "100%" }}>
+      <div
+        style={{
+          padding: "5%",
+          paddingBottom: "20%",
+          maxWidth: "100%",
+        }}
+      >
         <Outlet />
       </div>
-      <Footer style={{ position: "fixed", bottom: "0", width: "100%" }}>
+      <Footer
+        style={{
+          position: "fixed",
+          bottom: "0",
+          width: "100%",
+        }}
+      >
         <a
           href="https://github.com/abhay-raizada/nostr-forms"
           style={{ textDecoration: "none", color: "black" }}
