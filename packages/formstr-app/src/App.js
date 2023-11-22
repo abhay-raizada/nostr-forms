62709270efa8c8481e6909d5abeb5dbd90ef85a8
mayuri <ardardmayuri@gmail.com>
2023-11-22 09:36:47 +0530

    implement design layout for header

--- packages/formstr-app/src/App.js
+++ packages/formstr-app/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Header } from "./components/Header";
+import { NostrHeader } from "./components/Header";
 import { Outlet } from "react-router-dom";
 import { Layout } from "antd";
 import { GithubOutlined } from "@ant-design/icons";
@@ -8,7 +8,7 @@ function App() {
   const { Footer } = Layout;
   return (
     <div className="App">
-      <Header selected="Create Form" />
+      <NostrHeader selected="Create Form" />
       <div
         style={{
           padding: "5%",
