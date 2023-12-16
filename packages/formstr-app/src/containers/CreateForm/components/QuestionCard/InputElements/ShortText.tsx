0c11dbb17f848b8a1320bee55842ebea4689fa03
Ragautam <gautamkumarradadiya009@gmail.com>
2023-12-16 15:59:17 +0530

    comment unused code

--- packages/formstr-app/src/containers/CreateForm/components/QuestionCard/InputElements/ShortText.tsx
+++ packages/formstr-app/src/containers/CreateForm/components/QuestionCard/InputElements/ShortText.tsx
@@ -1,23 +1,23 @@
 import { Input } from "antd";
-import QuestionContext from "../../QuestionContext";
-import { AnswerTypes } from "@formstr/sdk/dist/interfaces";
+// import QuestionContext from "../../QuestionContext";
+// import { AnswerTypes } from "@formstr/sdk/dist/interfaces";
 
-{
-  /* <Input
-  placeholder="Type your answer here"
-  bordered={false}
-  disabled
-  style={{
-    paddingLeft: 0,
-    // borderBottom: "0.5px solid black",
-    borderRadius: 0,
-  }}
-/>; */
-}
+// {
+//   <Input
+//   placeholder="Type your answer here"
+//   bordered={false}
+//   disabled
+//   style={{
+//     paddingLeft: 0,
+//     // borderBottom: "0.5px solid black",
+//     borderRadius: 0,
+//   }}
+// />;
+// }
 
-interface ShortTextInputSettings {
-  validations: Array<string>;
-}
+// interface ShortTextInputSettings {
+//   validations: Array<string>;
+// }
 interface ShortTextProps {}
 
 const shortText: React.FC<ShortTextProps> = () => {
