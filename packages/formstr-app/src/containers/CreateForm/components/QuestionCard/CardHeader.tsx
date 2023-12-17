df4e07a05bf0cd5711a83e3c732be550e72e6a85
Abhay Raizada <toabhayraizada@gmail.com>
2023-12-17 23:02:33 +0530

    Make required button functional (#94)

--- packages/formstr-app/src/containers/CreateForm/components/QuestionCard/CardHeader.tsx
+++ packages/formstr-app/src/containers/CreateForm/components/QuestionCard/CardHeader.tsx
@@ -1,21 +1,38 @@
 import { ArrowDownOutlined, ArrowUpOutlined } from "@ant-design/icons";
 import { ReactComponent as Asterisk } from "../../../../Images/asterisk.svg";
+import StyledWrapper from "./style";
 
-const CardHeader = () => {
+interface CardHeaderProps {
+  required: boolean;
+  onRequired: (required: boolean) => void;
+}
+
+const CardHeader: React.FC<CardHeaderProps> = ({ required, onRequired }) => {
   return (
-    <div style={{ display: "flex", paddingBottom: 10 }}>
-      <div style={{ padding: 5, paddingLeft: 0 }}>
-        <ArrowUpOutlined style={{ fontSize: 12 }} />
+    <StyledWrapper>
+      <div style={{ display: "flex", paddingBottom: 10 }}>
+        <div style={{ padding: 5, cursor: "pointer", paddingLeft: 0 }}>
+          <ArrowUpOutlined style={{ fontSize: 12 }} />
+        </div>
+        <div style={{ padding: 5, cursor: "pointer" }}>
+          <ArrowDownOutlined style={{ fontSize: 12 }} />
+        </div>
+        <div style={{ padding: 5, cursor: "pointer" }}>
+          <Asterisk
+            style={{
+              fontSize: 12,
+              margin: "2.5px",
+              height: "1em",
+              width: "1em",
+            }}
+            className={!required ? "asterisk" : "asteriskSelected"}
+            onClick={() => {
+              onRequired(!required);
+            }}
+          />
+        </div>
       </div>
-      <div style={{ padding: 5 }}>
-        <ArrowDownOutlined style={{ fontSize: 12 }} />
-      </div>
-      <div style={{ padding: 5 }}>
-        <Asterisk
-          style={{ fontSize: 12, margin: "2.5px", height: "1em", width: "1em" }}
-        />
-      </div>
-    </div>
+    </StyledWrapper>
   );
 };
 
