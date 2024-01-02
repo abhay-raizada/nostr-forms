54ba72050a7d452b6927947db61bea8591545b90
mayuriardad <41021593+mayuriardad@users.noreply.github.com>
2024-01-02 12:56:28 +0530

    remove publish btn from mobile menu (#109)
    Co-authored-by: mayuri <ardardmayuri@gmail.com>

--- packages/formstr-app/src/containers/CreateForm/components/Header/Header.tsx
+++ packages/formstr-app/src/containers/CreateForm/components/Header/Header.tsx
@@ -2,14 +2,17 @@ import { Layout, Menu, Row, Col, Typography } from "antd";
 import { Link, useLocation } from "react-router-dom";
 import { ArrowLeftOutlined, MenuOutlined } from "@ant-design/icons";
 import { ROUTES } from "../../../../constants/routes";
-import { useHeaderConfig, HEADER_MENU_KEYS } from "./config";
+import { HEADER_MENU, HEADER_MENU_KEYS } from "./config";
+import { Button } from "antd";
+import useFormBuilderContext from "../../hooks/useFormBuilderContext";
+import StyleWrapper from "./Header.style";
 
 export const CreateFormHeader = () => {
   const location = useLocation();
   const { Header } = Layout;
   const { Text } = Typography;
 
-  const { HEADER_MENU } = useHeaderConfig();
+  const { saveForm } = useFormBuilderContext();
 
   const getSelectedTab = () => {
     if (location.pathname === ROUTES.RESPONSES) {
@@ -22,14 +25,8 @@ export const CreateFormHeader = () => {
   };
 
   return (
-    <>
-      <Header
-        className="header-style"
-        style={{
-          background: "white",
-          borderBottom: "1px solid #ddd",
-        }}
-      >
+    <StyleWrapper>
+      <Header className="create-form-header">
         <Row className="header-row" justify="space-between">
           <Col>
             <Row className="header-row" justify="space-between">
@@ -45,17 +42,27 @@ export const CreateFormHeader = () => {
               </Col>
             </Row>
           </Col>
-          <Col md={6} xs={2} sm={2}>
-            <Menu
-              mode="horizontal"
-              theme="light"
-              defaultSelectedKeys={[getSelectedTab()]}
-              overflowedIndicator={<MenuOutlined />}
-              items={HEADER_MENU}
-            />
+
+          <Col md={8} xs={10} sm={10}>
+            <Row className="header-row" justify="end">
+              <Col>
+                <Button type="primary" onClick={saveForm}>
+                  Publish
+                </Button>
+              </Col>
+              <Col md={10} xs={5} sm={2}>
+                <Menu
+                  mode="horizontal"
+                  theme="light"
+                  defaultSelectedKeys={[getSelectedTab()]}
+                  overflowedIndicator={<MenuOutlined />}
+                  items={HEADER_MENU}
+                />
+              </Col>
+            </Row>
           </Col>
         </Row>
       </Header>
-    </>
+    </StyleWrapper>
   );
 };
--- packages/formstr-app/src/containers/CreateForm/components/Header/config.tsx
+++ packages/formstr-app/src/containers/CreateForm/components/Header/config.tsx
@@ -1,35 +1,16 @@
-import { Button } from "antd";
-import { MY_FORM_MENU } from "../../../MyForms/configs/menuConfig";
-import { isMobile } from "../../../../utils/utility";
-import useFormBuilderContext from "../../hooks/useFormBuilderContext";
-
 export const HEADER_MENU_KEYS = {
   RESPONSES: "RESPONSES",
   PREVIEW: "PREVIEW",
   PUBLISH: "PUBLISH",
 };
 
-export const useHeaderConfig = () => {
-  const { saveForm } = useFormBuilderContext();
-
-  const HEADER_MENU = [
-    {
-      key: HEADER_MENU_KEYS.RESPONSES,
-      label: "Responses",
-    },
-    {
-      key: HEADER_MENU_KEYS.PREVIEW,
-      label: "Preview",
-      ...(isMobile() && { children: MY_FORM_MENU }),
-    },
-    {
-      key: HEADER_MENU_KEYS.PUBLISH,
-      label: (
-        <Button type="primary" onClick={saveForm}>
-          Publish
-        </Button>
-      ),
-    },
-  ];
-  return { HEADER_MENU };
-};
+export const HEADER_MENU = [
+  {
+    key: HEADER_MENU_KEYS.RESPONSES,
+    label: "Responses",
+  },
+  {
+    key: HEADER_MENU_KEYS.PREVIEW,
+    label: "Preview",
+  },
+];
