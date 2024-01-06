782b4aa46408c44ad198975ae05bcdf9db2607f1
abhay-raizada <toabhayraizada@gmail.com>
2024-01-06 13:27:35 +0530

    Working Responses

--- packages/formstr-app/src/containers/Responses/Responses.tsx
+++ packages/formstr-app/src/containers/Responses/Responses.tsx
@@ -1,10 +1,8 @@
 import { getFormResponses } from "@formstr/sdk";
 import { FormResponse } from "@formstr/sdk/dist/interfaces";
-import { Table, Typography } from "antd";
+import { Table } from "antd";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
-const { Text } = Typography;
 
 export const Responses = () => {
   const { formSecret } = useParams();
@@ -38,7 +36,7 @@ export const Responses = () => {
     return Object.keys(responses).map((key) => {
       return {
         key,
-        author: responses[key].authorName || "Anonymous",
+        author: responses[key].authorName,
         Submissions: responses[key].responses.length,
         responses: responses[key].responses,
       };
@@ -50,38 +48,35 @@ export const Responses = () => {
       <Table
         columns={columns}
         dataSource={getData()}
+        pagination={false}
         expandable={{
           expandedRowRender: (record) => {
+            let lastResponse = record.responses[record.responses.length - 1];
             return (
               <div>
-                {record.responses.map((response, index) => {
-                  return (
-                    <>
-                      <Text> Submission {index + 1}</Text>
-                      <Table
-                        columns={[
-                          {
-                            key: "question",
-                            title: "Question",
-                            dataIndex: "question",
-                          },
-                          {
-                            key: "answer",
-                            title: "Answer",
-                            dataIndex: "answer",
-                          },
-                        ]}
-                        dataSource={response.map((question) => {
-                          return {
-                            key: question.questionId,
-                            question: question.questionId,
-                            answer: question.answer,
-                          };
-                        })}
-                      />
-                    </>
-                  );
-                })}
+                <Table
+                  key={lastResponse[0].questionLabel + " Table"}
+                  columns={[
+                    {
+                      key: "question",
+                      title: "Question",
+                      dataIndex: "question",
+                    },
+                    {
+                      key: "answer",
+                      title: "Answer",
+                      dataIndex: "answer",
+                    },
+                  ]}
+                  dataSource={lastResponse.map((question) => {
+                    return {
+                      key: question.questionId,
+                      question: question.questionLabel,
+                      answer: question.answer,
+                    };
+                  })}
+                  pagination={false}
+                />
               </div>
             );
           },
