6907d9c16dae375acf8f77efc7631d5c87299894
abhay-raizada <toabhayraizada@gmail.com>
2024-01-11 21:06:44 +0530

    Add notifications

---
+++ packages/formstr-app/src/containers/CreateForm/components/FormSettings/addNpub.style.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export default styled.div`
+  .modal-container {
+    display: "flex";
+    margin: "10px";
+    flex-direction: "column";
+    align-items: "center";
+  }
+
+  .npub-input {
+    margin: 10px;
+  }
+
+  .error-npub {
+    color: red;
+    font-size: 12px;
+  }
+
+  .add-button {
+    margin: 10px;
+    width: 64px;
+  }
+`;
--- packages/formstr-sdk/src/interfaces/v1/index.ts
+++ packages/formstr-sdk/src/interfaces/v1/index.ts
@@ -125,4 +125,7 @@ export interface IFormSettings {
   titleImageUrl?: string;
   description?: string;
   thankYouPage?: boolean;
+  notifyNpubs?: Array<string>;
+  publicForm?: boolean;
+  disallowAnonymous?: boolean;
 }
