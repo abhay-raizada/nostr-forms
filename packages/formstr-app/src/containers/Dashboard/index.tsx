6c23f298af456e147ef34b95ba1fee21ef6536c2
Abhay Raizada <toabhayraizada@gmail.com>
2024-09-25 16:04:39 +0530

    Add Dropdown menu

--- packages/formstr-app/src/containers/Dashboard/index.tsx
+++ packages/formstr-app/src/containers/Dashboard/index.tsx
@@ -14,16 +14,24 @@ import { useApplicationContext } from "../../hooks/useApplicationContext";
 import { getItem, LOCAL_STORAGE_KEYS } from "../../utils/localStorage";
 import { ILocalForm } from "../CreateFormNew/providers/FormBuilder/typeDefs";
 import { LocalFormCard } from "./LocalFormCard";
+import { Dropdown, Menu, Typography } from "antd";
+import { DownOutlined } from "@ant-design/icons";
 
 const defaultRelays = getDefaultRelays();
 
 export const Dashboard = () => {
   const { state } = useLocation();
+  const { pubkey, requestPubkey } = useProfileContext();
+
   const [showFormDetails, setShowFormDetails] = useState<boolean>(!!state);
   const [localForms, setLocalForms] = useState<ILocalForm[]>(
     getItem(LOCAL_STORAGE_KEYS.LOCAL_FORMS) || []
   );
   const [nostrForms, setNostrForms] = useState<Event[] | undefined>(undefined);
+  const [filter, setFilter] = useState<"local" | "nostr">(
+    pubkey ? "nostr" : "local"
+  );
+
   const { poolRef } = useApplicationContext();
 
   const subCloserRef = useRef<SubCloser | null>(null);
@@ -33,8 +41,6 @@ export const Dashboard = () => {
       return [...(prevEvents || []), event];
     });
   };
-
-  const { pubkey, requestPubkey } = useProfileContext();
 
   const fetchNostrForms = () => {
     console.log("Inside fetchNostrForms");
@@ -68,36 +74,83 @@ export const Dashboard = () => {
     };
   }, [pubkey]);
 
+  const renderForms = () => {
+    if (filter === "local") {
+      return localForms.map((localForm: ILocalForm) => (
+        <LocalFormCard key={localForm.key} form={localForm} />
+      ));
+    } else if (filter === "nostr") {
+      return (nostrForms || []).map((formEvent: Event) => (
+        <FormEventCard key={formEvent.id} event={formEvent} />
+      ));
+    }
+    return null;
+  };
+
+  const menu = (
+    <Menu>
+      <Menu.Item key="local" onClick={() => setFilter("local")}>
+        Forms on this device
+      </Menu.Item>
+      <Menu.Item
+        key="nostr"
+        onClick={() => setFilter("nostr")}
+        disabled={!pubkey}
+      >
+        Forms on your Nostr profile
+      </Menu.Item>
+    </Menu>
+  );
+
   return (
     <DashboardStyleWrapper>
       <div className="dashboard-container">
-        {!pubkey && <LoggedOutScreen requestLogin={requestPubkey} />}
-        {pubkey && (
-          <div className="form-cards-container">
-            {localForms.map((localForm: ILocalForm) => {
-              return <LocalFormCard form={localForm} />;
-            })}
-          </div>
-        )}
-        {pubkey && (
-          <div className="form-cards-container">
-            {(nostrForms || []).map((formEvent: Event) => {
-              return <FormEventCard event={formEvent} />;
-            })}
-          </div>
-        )}
-        {pubkey && !nostrForms?.length && !localForms.length ? (
-          <EmptyScreen />
+        <div style={{ margin: 10 }}>
+          <Dropdown overlay={menu} trigger={["click"]} placement="bottomLeft">
+            <div
+              style={{
+                color: "grey",
+                cursor: "pointer",
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              {filter === "local" ? (
+                <Typography.Text> Forms on this device </Typography.Text>
+              ) : (
+                <Typography.Text>
+                  Forms linked to your Nostr profile
+                </Typography.Text>
+              )}
+              <DownOutlined
+                style={{ marginLeft: "8px", fontSize: "12px", marginTop: -5 }}
+              />
+            </div>
+          </Dropdown>
+        </div>
+        {!pubkey && localForms.length !== 0 ? (
+          <div className="form-cards-container">{renderForms()}</div>
         ) : null}
-        {showFormDetails && (
-          <FormDetails
-            isOpen={showFormDetails}
-            pubKey={state.pubKey}
-            secretKey={state.secretKey}
-            viewKey={state.viewKey}
-            formId={state.formId}
-            onClose={() => setShowFormDetails(false)}
-          />
+
+        {!pubkey && localForms.length === 0 && (
+          <LoggedOutScreen requestLogin={requestPubkey} />
+        )}
+
+        {pubkey && (
+          <>
+            <div className="form-cards-container">{renderForms()}</div>
+            {!nostrForms?.length && !localForms.length ? <EmptyScreen /> : null}
+            {showFormDetails && (
+              <FormDetails
+                isOpen={showFormDetails}
+                pubKey={state.pubKey}
+                secretKey={state.secretKey}
+                viewKey={state.viewKey}
+                formId={state.formId}
+                onClose={() => setShowFormDetails(false)}
+              />
+            )}
+          </>
         )}
       </div>
     </DashboardStyleWrapper>
