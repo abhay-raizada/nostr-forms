1fc4d3d9ce369b98490a0d1cb4c4cf5fb219c6ea
abhay-raizada <toabhayraizada@gmail.com>
2024-11-17 23:54:08 +0530

    Responses: Fix Fields (#181)
    * Fix Author and widths in responses
    
    * Bring back export as excel
    
    * work with old urls

--- packages/formstr-app/src/containers/ResponsesNew/Export.tsx
+++ packages/formstr-app/src/containers/ResponsesNew/Export.tsx
@@ -4,31 +4,13 @@ import { DownOutlined } from "@ant-design/icons";
 import { V1Field } from "@formstr/sdk/dist/interfaces";
 
 export const Export: React.FC<{
-  questionMap: { [key: string]: V1Field };
-  answers: Array<{ [key: string]: string }>;
+  responsesData: Array<{ [key: string]: string }>;
   formName: string;
-}> = (props) => {
+}> = ({ responsesData, formName }) => {
   const onDownloadClick = async (type: "csv" | "excel") => {
     const XLSX = await import("xlsx");
-    const SheetName =
-      `Responses for ${props.formName}`.substring(0, 16) + "...";
-    let parsedResponse = props.answers.reduce(
-      (
-        allResp: Array<{ [key: string]: string }>,
-        answer: { [key: string]: string }
-      ) => {
-        let newAnswer: { [key: string]: string } = {};
-        Object.keys(answer).forEach((questionId) => {
-          newAnswer[props.questionMap[questionId]?.question || questionId] =
-            answer[questionId];
-        });
-        allResp.push(newAnswer);
-        return allResp;
-      },
-      []
-    );
-
-    const workSheet = XLSX.utils.json_to_sheet(parsedResponse);
+    const SheetName = `Responses for ${formName}`.substring(0, 16) + "...";
+    const workSheet = XLSX.utils.json_to_sheet(responsesData);
     const workBook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(workBook, workSheet, `${SheetName}`);
     if (type === "excel") {
